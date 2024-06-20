4c6324cebcb355386e380b61b5f269375c58f6ba
Jay <jlaura@usgs.gov>
2024-06-20 14:12:27 -0500

    Bumps the version to dev, since this is dev.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 with open('README.md', 'r') as f:
     long_description = f.read()
 
-__version__ = '1.2.1'
+__version__ = '1.2.1-dev'
 
 def setup_package():
     setup(
